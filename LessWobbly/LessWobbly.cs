3094ef50a1d6d4105b912db67963a42e2f44a6d2
Jonathan Bayer <linuxgurugamer@gmail.com>
2023-02-26 13:34:12 -0500

    Added Readme.md

---
+++ LessWobbly/LessWobbly.cs
@@ -0,0 +1,83 @@
+﻿using BepInEx;
+using KSP.Game;
+using UnityEngine;
+
+using System.Collections.Generic;
+using System.Linq;
+using KSP.IO;
+using KSP.Sim.impl;
+
+namespace LazyOrbit
+{
+    [BepInPlugin("io.bepis.LessWobbly", "LessWobbly", "1.0")]
+    public class LessWobbly : BaseUnityPlugin
+    {
+        static bool loaded = false;
+        private bool drawUI = false;
+        private Rect windowRect;
+        private int windowWidth = 350;
+        private int windowHeight = 700;
+
+
+        private static GUIStyle boxStyle;
+
+        float lessWobbly = PhysicsSettings.JOINT_RIGIDITY;
+
+        public  void Initialize()
+        {
+            if (loaded)
+            {
+                Destroy(this);
+            }
+
+            loaded = true;
+            lessWobbly = PhysicsSettings.JOINT_RIGIDITY;
+        }
+
+        void Awake()
+        {
+            Initialize();
+
+            windowRect = new Rect((Screen.width * 0.85f) - (windowWidth / 2), (Screen.height / 2) - (windowHeight / 2), 0, 0);
+        }
+
+        void OnGUI()
+        {
+            if (drawUI)
+            {
+                windowRect = GUILayout.Window(
+                    GUIUtility.GetControlID(FocusType.Passive),
+                    windowRect,
+                    FillWindow,
+                    "Lazy Orbit",
+                    GUILayout.Height(0),
+                    GUILayout.Width(350));
+            }
+        }
+
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.J))
+                drawUI = !drawUI;
+        }
+
+        private void FillWindow(int windowID)
+        {
+            boxStyle = GUI.skin.GetStyle("Box");
+            GUILayout.BeginVertical();
+
+            GUILayout.Label($"Joint Rigidity: {PhysicsSettings.JOINT_RIGIDITY}");
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label($"{lessWobbly}:");
+            lessWobbly = GUILayout.HorizontalSlider(lessWobbly, 1500f, 1500000f);
+            GUILayout.EndHorizontal();
+
+            if (GUILayout.Button("Apply"))
+                PhysicsSettings.JOINT_RIGIDITY = lessWobbly;
+
+            GUILayout.EndVertical();
+            GUI.DragWindow(new Rect(0, 0, 10000, 500));
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,13 @@
+This mod will let you change the JOINT_RIGIDITY setting, which may help the
+wobbliness of rockets
+
+Install
+
+1. Download and install BepInEx 5
+2. Download and extract this mod into your KSP 2 install folder (usually "Kerbal Space Program 2")
+
+
+Usage
+
+To open window, use Alt-J, use the slider and then hit apply
+To close the window, use Alt-J again
