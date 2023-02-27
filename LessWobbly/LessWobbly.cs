d659c9b86be1d81288a187166ae0b188c8cf53fd
Jonathan Bayer <linuxgurugamer@gmail.com>
2023-02-27 14:14:00 -0500

    Fixed title  Added JointBreakForceFactor  Added Reset button  Added automatic saving/loading of entered data

--- LessWobbly/LessWobbly.cs
+++ LessWobbly/LessWobbly.cs
@@ -1,18 +1,38 @@
 ﻿using BepInEx;
 using KSP.Game;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 using System.Collections.Generic;
 using System.Linq;
 using KSP.IO;
 using KSP.Sim.impl;
+using System.Linq.Expressions;
+using System;
 
-namespace LazyOrbit
+namespace LessWobbly
 {
+    internal class WobblySettings
+    {
+        [SerializeField]
+        internal float jointRigidity = 1500f;
+        [SerializeField]
+        internal float jointBreakForceFactor = 50f;
+
+        internal WobblySettings(float jointRigidity, float jointBreakForceFactor)
+        {
+            this.jointRigidity = jointRigidity;
+            this.jointBreakForceFactor = jointBreakForceFactor;
+        }
+
+        internal WobblySettings() { }
+    }
+
+
     [BepInPlugin("io.bepis.lesswobbly", "LessWobbly", "1.0")]
     public class LessWobbly : BaseUnityPlugin
     {
-        static bool loaded = false;
+        static bool loadedData = false;
         private bool drawUI = false;
         private Rect windowRect;
         private int windowWidth = 350;
@@ -21,22 +41,16 @@ namespace LazyOrbit
 
         private static GUIStyle boxStyle;
 
-        float lessWobbly = 1500f;
+        WobblySettings wobblySettings;
 
-        public  void Initialize()
+        internal void Initialize(WobblySettings lw)
         {
-            if (loaded)
-            {
-                Destroy(this);
-            }
-
-            loaded = true;
-            //lessWobbly = PhysicsSettings.JOINT_RIGIDITY;
+            wobblySettings = new WobblySettings(lw.jointRigidity, lw.jointBreakForceFactor);
         }
 
         void Awake()
         {
-            Initialize();
+            LoadWobblySettings(false);
 
             windowRect = new Rect((Screen.width * 0.85f) - (windowWidth / 2), (Screen.height / 2) - (windowHeight / 2), 0, 0);
         }
@@ -49,16 +63,64 @@ namespace LazyOrbit
                     GUIUtility.GetControlID(FocusType.Passive),
                     windowRect,
                     FillWindow,
-                    "Lazy Orbit",
+                    "Less Wobbly",
                     GUILayout.Height(0),
                     GUILayout.Width(350));
             }
         }
 
+        public void LoadWobblySettings(bool create)
+        {
+            bool wobblySettingsFileExists = IOProvider.JsonFileExists(IOProvider.DataLocation.Global, "WobblySetting");
+            WobblySettings parsedValue;
+            if (wobblySettingsFileExists)
+            {
+                if (!IOProvider.FromJsonFile<WobblySettings>(IOProvider.DataLocation.Global, "WobblySetting", out parsedValue))
+                {
+                    parsedValue = new WobblySettings();
+                }
+            }
+            else
+            {
+                parsedValue = new WobblySettings();
+            }
+            Initialize(parsedValue);
+            //if (create)
+            //    SaveData();
+        }
+
+        void SaveData()
+        {
+            WobblySettings parsedValue = new WobblySettings();
+            parsedValue.jointBreakForceFactor = wobblySettings.jointBreakForceFactor;
+            parsedValue.jointRigidity = wobblySettings.jointRigidity;
+            IOProvider.ToJsonFile(IOProvider.DataLocation.Global, "WobblySetting", parsedValue);
+        }
+
+        int cnt = 0;
         void Update()
         {
+            if (HighLogic.LoadedScene == GameScenes.LOADING && !loadedData && cnt > 10)
+            {
+                LoadWobblySettings(false);
+                try
+                {
+                    PhysicsSettings.JOINT_RIGIDITY = wobblySettings.jointRigidity;
+                    PhysicsSettings.JointBreakForceFactor = wobblySettings.jointBreakForceFactor;
+                    loadedData = true;
+                }
+                catch (NullReferenceException e)
+                {
+                    cnt = 0;
+                }
+            }
+            if (!loadedData)
+                cnt++;
+
             if ((Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt)) && Input.GetKeyDown(KeyCode.J))
+            {
                 drawUI = !drawUI;
+            }
         }
 
         private void FillWindow(int windowID)
@@ -69,13 +131,33 @@ namespace LazyOrbit
             GUILayout.Label($"Joint Rigidity: {PhysicsSettings.JOINT_RIGIDITY}");
 
             GUILayout.BeginHorizontal();
-            GUILayout.Label($"{lessWobbly}:");
-            lessWobbly = GUILayout.HorizontalSlider(lessWobbly, 1500f, 1500000f);
+            GUILayout.Label($"{wobblySettings.jointRigidity}:", GUILayout.Width(50));
+            wobblySettings.jointRigidity = GUILayout.HorizontalSlider(wobblySettings.jointRigidity, 1500f, 1500000f);
             GUILayout.EndHorizontal();
 
-            if (GUILayout.Button("Apply"))
-                PhysicsSettings.JOINT_RIGIDITY = lessWobbly;
+            GUILayout.Label($"Joint Break Force Factor: {PhysicsSettings.JointBreakForceFactor}");
 
+            GUILayout.BeginHorizontal();
+            GUILayout.Label($"{wobblySettings.jointBreakForceFactor}:", GUILayout.Width(50));
+            wobblySettings.jointBreakForceFactor = GUILayout.HorizontalSlider(wobblySettings.jointBreakForceFactor, 10f, 50f);
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("Reset"))
+            {
+                wobblySettings = new WobblySettings();
+            }
+            GUILayout.FlexibleSpace();
+
+            if (GUILayout.Button("Apply"))
+            {
+                PhysicsSettings.JOINT_RIGIDITY = wobblySettings.jointRigidity;
+                PhysicsSettings.JointBreakForceFactor = wobblySettings.jointBreakForceFactor;
+                SaveData();
+            }
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
             GUILayout.EndVertical();
             GUI.DragWindow(new Rect(0, 0, 10000, 500));
         }
